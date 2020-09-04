dd330025f3b34c208edc2abc8f36ab2e180fcd27
Cesar <cesardmn@gmail.com>
2020-09-04 18:47:25 -0300

    start hot-cards

--- src/pages/FitPage/index.tsx
+++ src/pages/FitPage/index.tsx
@@ -1,6 +1,11 @@
 import React from 'react'
 
+import './styles.css'
+
 import Header from '../../components/Header'
+
+import cardImg from '../../assets/img/logo_fundo.png'
+
 
 function FitPage() {
   return (
@@ -8,8 +13,114 @@ function FitPage() {
 
       <Header />
 
-      <h3>Refeições Fit</h3>
-      <p>Refeição individual - embalagem de 500ml</p>
+      <div className="top-page">
+        <h1 className="title" >Refeições Fit</h1>
+        <h3 className="information" >Refeição individual (embalagem de 500ml).</h3>
+      </div>
+
+      <div className="hot-cards">
+
+        <h2>Destaques</h2>
+
+        <div className="cards">
+
+          <div className="card">
+
+
+            <img src={cardImg} alt="" />
+
+
+            <p>
+              Carne louca desfiada com molho de tomate e cebola, legumes com especiarias e arroz integral.
+            </p>
+
+
+            <div className="price">
+              <div className="sale">
+                <span>R$ </span> <span>112,90</span>
+              </div>
+              <div className="full">
+                <span>R$ </span> <span>1.1115,00</span>
+              </div>
+            </div>
+
+          </div>
+
+          <div className="card">
+
+
+            <img src={cardImg} alt="" />
+
+
+            <p>
+              Carne louca desfiada com molho de tomate e cebola, legumes com especiarias e arroz integral.
+            </p>
+
+
+            <div className="price">
+              <div className="sale">
+                <span>R$ </span> <span>112,90</span>
+              </div>
+              <div className="full">
+                <span>R$ </span> <span>1.1115,00</span>
+              </div>
+            </div>
+
+          </div>
+
+          <div className="card">
+
+
+            <img src={cardImg} alt="" />
+
+
+            <p>
+              Carne louca desfiada com molho de tomate e cebola, legumes com especiarias e arroz integral.
+            </p>
+
+
+            <div className="price">
+              <div className="sale">
+                <span>R$ </span> <span>112,90</span>
+              </div>
+              <div className="full">
+                <span>R$ </span> <span>1.1115,00</span>
+              </div>
+            </div>
+
+          </div>
+
+          <div className="card">
+
+
+            <img src={cardImg} alt="" />
+
+
+            <p>
+              Carne louca desfiada com molho de tomate e cebola, legumes com especiarias e arroz integral.
+            </p>
+
+
+            <div className="price">
+              <div className="sale">
+                <span>R$ </span> <span>112,90</span>
+              </div>
+              <div className="full">
+                <span>R$ </span> <span>1.1115,00</span>
+              </div>
+            </div>
+
+          </div>
+
+        </div>
+
+
+
+      </div>
+
+      <div className="list-cards">
+
+      </div>
 
 
     </div>
