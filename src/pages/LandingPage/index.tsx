6470d946620230b2719e0bd74e5bf171922ed64d
Cesar <cesardmn@gmail.com>
2020-09-04 15:47:51 -0300

    create Landing Page

--- src/pages/LandingPage/index.tsx
+++ src/pages/LandingPage/index.tsx
@@ -1,8 +1,52 @@
 import React from 'react'
 
-function LandingPage(){
+import './styles.css'
+
+import logoImg from '../../assets/img/logo.png'
+
+
+function LandingPage() {
   return (
-    <h1>LandingPage</h1>
+    <div id="landing">
+
+
+      <div className="hero">
+        <img src={logoImg} alt="" />
+        <h3>"Do Fogão para sua mesa!"</h3>
+      </div>
+
+      <div className="menu">
+        <nav>
+        <li>
+          <a href="">
+            <i className="fas fa-utensils"></i>
+            <span>Combos</span>
+          </a>
+        </li>
+
+        <li>
+          <a href="">
+            <i className="fas fa-carrot"></i>
+            <span>Fit</span>
+          </a>
+        </li>
+
+        <li>
+          <a href="">
+            <i className="fas fa-drumstick-bite"></i>
+            <span>Low Carb</span>
+          </a>
+        </li>
+
+        <li>
+          <a href="">
+            <i className="fas fa-shopping-cart"></i>
+            <span>Pedidos</span>
+          </a>
+        </li>
+        </nav>
+      </div>
+    </div>
   )
 }
 
