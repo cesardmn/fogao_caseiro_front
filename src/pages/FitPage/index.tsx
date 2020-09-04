70d97028c474d029977534d73c72153cc4e267fc
Cesar <cesardmn@gmail.com>
2020-09-04 19:18:49 -0300

    start list horizon cards

--- src/pages/FitPage/index.tsx
+++ src/pages/FitPage/index.tsx
@@ -18,108 +18,229 @@ function FitPage() {
         <h3 className="information" >Refeição individual (embalagem de 500ml).</h3>
       </div>
 
-      <div className="hot-cards">
+      <div className="content">
+        <div className="hot-cards">
 
-        <h2>Destaques</h2>
+          <h2>Destaques</h2>
 
-        <div className="cards">
+          <div className="cards">
 
-          <div className="card">
-
-
-            <img src={cardImg} alt="" />
+            <div className="card">
 
 
-            <p>
-              Carne louca desfiada com molho de tomate e cebola, legumes com especiarias e arroz integral.
+              <img src={cardImg} alt="" />
+
+
+              <p>
+                Carne louca desfiada com molho de tomate e cebola, legumes com especiarias e arroz integral.
             </p>
 
 
-            <div className="price">
-              <div className="sale">
-                <span>R$ </span> <span>112,90</span>
+              <div className="price">
+                <div className="sale">
+                  <span>R$ </span> <span>112,90</span>
+                </div>
+                <div className="full">
+                  <span>R$ </span> <span>1.1115,00</span>
+                </div>
               </div>
-              <div className="full">
-                <span>R$ </span> <span>1.1115,00</span>
+
+            </div>
+
+            <div className="card">
+
+
+              <img src={cardImg} alt="" />
+
+
+              <p>
+                Carne louca desfiada com molho de tomate e cebola, legumes com especiarias e arroz integral.
+              </p>
+
+
+              <div className="price">
+                <div className="sale">
+                  <span>R$ </span> <span>112,90</span>
+                </div>
+                <div className="full">
+                  <span>R$ </span> <span>1.1115,00</span>
+                </div>
               </div>
+
+            </div>
+
+            <div className="card">
+
+
+              <img src={cardImg} alt="" />
+
+
+              <p>
+                Carne louca desfiada com molho de tomate e cebola, legumes com especiarias e arroz integral.
+            </p>
+
+
+              <div className="price">
+                <div className="sale">
+                  <span>R$ </span> <span>112,90</span>
+                </div>
+                <div className="full">
+                  <span>R$ </span> <span>1.1115,00</span>
+                </div>
+              </div>
+
+            </div>
+
+            <div className="card">
+
+
+              <img src={cardImg} alt="" />
+
+
+              <p>
+                Carne louca desfiada com molho de tomate e cebola, legumes com especiarias e arroz integral.
+            </p>
+
+
+              <div className="price">
+                <div className="sale">
+                  <span>R$ </span> <span>112,90</span>
+                </div>
+                <div className="full">
+                  <span>R$ </span> <span>1.1115,00</span>
+                </div>
+              </div>
+
             </div>
 
           </div>
 
-          <div className="card">
 
 
-            <img src={cardImg} alt="" />
-
-
-            <p>
-              Carne louca desfiada com molho de tomate e cebola, legumes com especiarias e arroz integral.
-            </p>
-
-
-            <div className="price">
-              <div className="sale">
-                <span>R$ </span> <span>112,90</span>
-              </div>
-              <div className="full">
-                <span>R$ </span> <span>1.1115,00</span>
-              </div>
-            </div>
-
-          </div>
-
-          <div className="card">
-
-
-            <img src={cardImg} alt="" />
-
-
-            <p>
-              Carne louca desfiada com molho de tomate e cebola, legumes com especiarias e arroz integral.
-            </p>
-
-
-            <div className="price">
-              <div className="sale">
-                <span>R$ </span> <span>112,90</span>
-              </div>
-              <div className="full">
-                <span>R$ </span> <span>1.1115,00</span>
-              </div>
-            </div>
-
-          </div>
-
-          <div className="card">
-
-
-            <img src={cardImg} alt="" />
-
-
-            <p>
-              Carne louca desfiada com molho de tomate e cebola, legumes com especiarias e arroz integral.
-            </p>
-
-
-            <div className="price">
-              <div className="sale">
-                <span>R$ </span> <span>112,90</span>
-              </div>
-              <div className="full">
-                <span>R$ </span> <span>1.1115,00</span>
-              </div>
-            </div>
-
-          </div>
 
         </div>
 
+        <div className="list-cards">
+
+          <div className="horizon-card">
+
+            <img src={cardImg} alt="" />
 
 
-      </div>
+            <div className="right">
+              <p>
+                Carne louca desfiada com molho de tomate e cebola, legumes com especiarias e arroz integral.
+            </p>
 
-      <div className="list-cards">
 
+              <div className="price">
+                <div className="sale">
+                  <span>R$ </span> <span>112,90</span>
+                </div>
+                <div className="full">
+                  <span>R$ </span> <span>1.1115,00</span>
+                </div>
+              </div>
+
+            </div>
+
+          </div>
+
+          <div className="horizon-card">
+
+            <img src={cardImg} alt="" />
+
+
+            <div className="right">
+              <p>
+                Carne louca desfiada com molho de tomate e cebola, legumes com especiarias e arroz integral.
+              </p>
+
+
+              <div className="price">
+                <div className="sale">
+                  <span>R$ </span> <span>112,90</span>
+                </div>
+                <div className="full">
+                  <span>R$ </span> <span>1.1115,00</span>
+                </div>
+              </div>
+
+            </div>
+
+          </div>
+
+          <div className="horizon-card">
+
+            <img src={cardImg} alt="" />
+
+
+            <div className="right">
+              <p>
+                Carne louca desfiada com molho de tomate e cebola, legumes com especiarias e arroz integral.
+</p>
+
+
+              <div className="price">
+                <div className="sale">
+                  <span>R$ </span> <span>112,90</span>
+                </div>
+                <div className="full">
+                  <span>R$ </span> <span>1.1115,00</span>
+                </div>
+              </div>
+
+            </div>
+
+          </div>          <div className="horizon-card">
+
+            <img src={cardImg} alt="" />
+
+
+            <div className="right">
+              <p>
+                Carne louca desfiada com molho de tomate e cebola, legumes com especiarias e arroz integral.
+</p>
+
+
+              <div className="price">
+                <div className="sale">
+                  <span>R$ </span> <span>112,90</span>
+                </div>
+                <div className="full">
+                  <span>R$ </span> <span>1.1115,00</span>
+                </div>
+              </div>
+
+            </div>
+
+          </div>          <div className="horizon-card">
+
+            <img src={cardImg} alt="" />
+
+
+            <div className="right">
+              <p>
+                Carne louca desfiada com molho de tomate e cebola, legumes com especiarias e arroz integral.
+</p>
+
+
+              <div className="price">
+                <div className="sale">
+                  <span>R$ </span> <span>112,90</span>
+                </div>
+                <div className="full">
+                  <span>R$ </span> <span>1.1115,00</span>
+                </div>
+              </div>
+
+            </div>
+
+          </div>
+
+
+
+        </div>
       </div>
 
 
