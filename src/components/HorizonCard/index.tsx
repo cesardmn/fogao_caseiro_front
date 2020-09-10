f98af680e54e7e38d2a36bd82f8f6d4a89c89e65
Cesar <cesardmn@gmail.com>
2020-09-10 14:04:10 -0300

    include link in HorizonCard and HotCard

--- src/components/HorizonCard/index.tsx
+++ src/components/HorizonCard/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import './styles.css'
 import cardImg from '../../assets/img/logo_fundo.png'
+import { Link } from 'react-router-dom'
 
 interface HorizonCardProps {
   information: {
+    id: string,
     title: string,
     name: string,
     price: number,
@@ -15,70 +17,72 @@ interface HorizonCardProps {
 
 const HorizonCard: React.FC<HorizonCardProps> = ({ information }) => {
   return (
-    <div className="horizon-card">
+    <Link to={`/${information.title}/${information.id}`}>
+      <div className="horizon-card">
 
-      <img src={cardImg} alt="" />
+        <img src={cardImg} alt="" />
 
 
-      <div className="right">
-        <h2>{information.title}</h2>
-        <p>{information.name}</p>
+        <div className="right">
+         <h2>{information.title}</h2>
+          <p>{information.name}</p>
 
-        <div className="price">
-          {
-            !information.hot &&
-            <div className="sale">
-              <span>
-                {(information.price).toLocaleString(
-                  'pt-BR', {
-                  minimumFractionDigits: 2,
-                  style: 'currency',
-                  currency: 'BRL'
-                }
-                )}
-              </span>
-            </div>
-          }
+          <div className="price">
+            {
+              !information.hot &&
+              <div className="sale">
+                <span>
+                  {(information.price).toLocaleString(
+                    'pt-BR', {
+                    minimumFractionDigits: 2,
+                    style: 'currency',
+                    currency: 'BRL'
+                  }
+                  )}
+                </span>
+              </div>
+            }
 
-          {
-            information.hot &&
-            <div className="sale">
-              <span>
-                {
-                  (information.price * 0.9)
-                    .toLocaleString(
-                      'pt-BR', {
-                      minimumFractionDigits: 2,
-                      style: 'currency',
-                      currency: 'BRL'
-                    }
-                    )
-                }
-              </span>
-            </div>
-          }
+            {
+              information.hot &&
+              <div className="sale">
+                <span>
+                  {
+                    (information.price * 0.9)
+                      .toLocaleString(
+                        'pt-BR', {
+                        minimumFractionDigits: 2,
+                        style: 'currency',
+                        currency: 'BRL'
+                      }
+                      )
+                  }
+                </span>
+              </div>
+            }
 
-          {
-            information.hot &&
-            <div className="full">
-              <span>
-                {(information.price).toLocaleString(
-                  'pt-BR', {
-                  minimumFractionDigits: 2,
-                  style: 'currency',
-                  currency: 'BRL'
-                }
-                )}
-              </span>
-            </div>
-          }
+            {
+              information.hot &&
+              <div className="full">
+                <span>
+                  {(information.price).toLocaleString(
+                    'pt-BR', {
+                    minimumFractionDigits: 2,
+                    style: 'currency',
+                    currency: 'BRL'
+                  }
+                  )}
+                </span>
+              </div>
+            }
+
+          </div>
+
 
         </div>
 
-
       </div>
-
-    </div>
+    </Link>
   )
 
 }
--- src/components/HotCardList/index.tsx
+++ src/components/HotCardList/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './styles.css'
 import HotCard from '../HotCard'
+import { Link } from 'react-router-dom'
 
 
 
@@ -12,27 +13,35 @@ function HotCardList() {
   const lowcarbs = (db.lowcarb).filter((item: { hot: string }) => item.hot !== '')
 
   return (
-      <>
-        {
-          [fits.length > 0, lowcarbs.length > 0] &&
-          <div className="hot-cards">
+    <>
+      {
+        [fits.length > 0, lowcarbs.length > 0] &&
+        <div className="hot-cards">
 
           <h2>Destaques</h2>
-    
+
           <div className="cards">
             {
-              fits.map((fit: { name: string, price: number, title: string }) => <HotCard key={fit.name} information={fit} />)
+              fits.map((fit: { id: string, name: string, price: number, title: string }) =>
+                <Link  key={fit.id} to={`/${fit.title}/${fit.id}`}>
+                  <HotCard key={fit.id} information={fit} />
+                </Link>
+              )
             }
-    
+
             {
-              lowcarbs.map((lowcarb: { name: string, price: number, title: string }) => <HotCard key={lowcarb.name} information={lowcarb} />)
+              lowcarbs.map((lowcarb: { id: string, name: string, price: number, title: string }) =>
+                <Link  key={lowcarb.id} to={`/${lowcarb.title}/${lowcarb.id}`}>
+                  <HotCard key={lowcarb.id} information={lowcarb} />
+                </Link>
+              )
             }
-    
+
           </div>
-    
+
         </div>
-        }
-      </>
+      }
+    </>
 
   )
 }
