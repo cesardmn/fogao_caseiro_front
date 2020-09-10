3d4558b25ea04fe64292be06f8eb3c2fdbaeb7d4
Cesar <cesardmn@gmail.com>
2020-09-10 17:36:43 -0300

    create currency formater

---
+++ src/utils/Utils.ts
@@ -0,0 +1,12 @@
+const stringToBRL = (value: number) =>{
+  return value.toLocaleString(
+    'pt-BR', {
+    minimumFractionDigits: 2,
+    style: 'currency',
+    currency: 'BRL'
+  })
+}
+
+
+export default stringToBRL
+
