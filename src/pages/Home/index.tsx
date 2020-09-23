cb5588498015e9d078c7e4d8819e9d982a34cb4d
Cesar <cesardmn@gmail.com>
2020-09-23 10:28:34 -0300

    include home page

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import Hero from '../../components/Hero'
+
+import './styles.css'
+
+function HomePage() {
+  return (
+
+    <div id="home-page">
+
+      <Hero />
+
+      <div className="description">
+        <p>
+          Aquela refeição, quentinha, na medida, para você e sua família e com um tempero caseiro que faz toda diferença.
+      </p>
+
+        <p>
+          E se ela for Fit ou Low Carb?
+      </p>
+
+        <p>
+          Será assim, especial! Até porque tempero caseiro sem conservantes e corantes não estraga sua alimentação.
+
+      </p>
+
+      </div>
+
+      <h1>Em Breve</h1>
+
+    </div>
+
+  )
+}
+
+export default HomePage
