7934dc40cbb5134db21e29b9dd952331caf40a16
Cesar <cesardmn@gmail.com>
2020-09-25 15:42:48 -0300

    version 0.0.5

---
+++ build/precache-manifest.4d57b345f74731a8e63ebc66ccf04ad3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4707350d8d61dd6e749d78a983a2466",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f12177267aebd57c52d8",
+    "url": "./static/css/main.4776b0e9.chunk.css"
+  },
+  {
+    "revision": "bf5c209f0c7052a157cf",
+    "url": "./static/js/2.5adf6701.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.5adf6701.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f12177267aebd57c52d8",
+    "url": "./static/js/main.e0349a7d.chunk.js"
+  },
+  {
+    "revision": "36c064ec115d4c18715b",
+    "url": "./static/js/runtime-main.6d9f6d63.js"
+  },
+  {
+    "revision": "654fde33b6c41f56f48f3a4f6b8d7675",
+    "url": "./static/media/logo.654fde33.png"
+  },
+  {
+    "revision": "d50e05c269abbc4f9e1e0f94d601f911",
+    "url": "./static/media/logo_fundo.d50e05c2.png"
+  }
+]);
