ae1f214b2a1cba83dc494a96e432a7801f85abd8
Cesar <cesardmn@gmail.com>
2020-10-06 19:47:46 -0300

    new Loading Page

--- src/pages/LoadingPage/index.tsx
+++ src/pages/LoadingPage/index.tsx
@@ -1,19 +1,19 @@
 import React from 'react'
 
 import './styles.css'
+import logoImg from '../../assets/img/logo.png'
 
-import Hero from '../../components/Hero'
 import Spinner from '../../components/Spinner'
 
 function LoadingPage() {
   return (
     <div id="loading">
 
-      <Hero />
+      <img src={logoImg} alt="logo fogão caseiro"/>
 
-      <>
+      <h1>Carregando um cardápio pra você...</h1>
+      
       <Spinner />
-      </>
 
     </div>
   )
