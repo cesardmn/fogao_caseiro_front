7122e56b6497c5050da30c3413c05635ce87e92d
Cesar <cesardmn@gmail.com>
2020-10-20 12:15:22 -0300

    styles choice items

---
+++ src/Utils/index.ts
@@ -0,0 +1,10 @@
+export default class Utils {
+  static numberToCurrencyBRL (value: number) {
+    return value.toLocaleString(
+      'pt-BR', {
+      minimumFractionDigits: 2,
+      style: 'currency',
+      currency: 'BRL'
+    })
+  }
+}
--- src/pages/ChoicePage/index.tsx
+++ src/pages/ChoicePage/index.tsx
@@ -9,6 +9,8 @@ import { IoIosArrowBack } from 'react-icons/io';
 
 import './styles.css'
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import Utils from '../../Utils';
 
 interface ParamsProps {
   id: string,
@@ -19,6 +21,9 @@ const ChoicePage = () => {
 
   const { id, name } = useParams<ParamsProps>()
   const toChoices = id ? DB.getCombo(id) : DB.getLunch(name)
+
+  const [totalAmount, setTotalAmount] = useState(0)
+
 
   return (
     <PageDefault >
@@ -38,39 +43,63 @@ const ChoicePage = () => {
 
         </div>
 
-        {
-          toChoices.choices.map((choice: any, index: number) => {
-            return (
-              <div className="choice-list" key={index}>
+        <div className="choice-list-container">
 
-                <div className="top">
-                  <div className="left">
-                    <h2 className="title">{choice.name}</h2>
-                    <p className="amount">escolha {choice.choice_amount} {choice.choice_amount > 1 ? 'opções' : 'opção'}</p>
+
+          {
+            toChoices.choices.map((choice: any, index: number) => {
+              return (
+                <div className="choice-list" key={index}>
+
+                  <div className="top">
+                    <div className="left">
+                      <h2 className="title">{choice.name}</h2>
+                      <p className="amount">escolha {choice.choice_amount} {choice.choice_amount > 1 ? 'opções' : 'opção'}</p>
+                    </div>
+
+                    <div className="right">
+                      <span>{totalAmount}/{choice.choice_amount}</span>
+                      <span>obrigatório</span>
+                    </div>
                   </div>
 
-                  <div className="right">
-                    <span>0/{choice.choice_amount}</span>
-                    <span>obrigatório</span>
-                  </div>
+                  <ul className="items-list">
+                    {
+                      choice.items.map((item: any, index: number) => {
+                        return (
+                          <li key={index} className="item">
+                            <div className="description">
+                              
+                              <p><strong>{item.id}</strong> - {item.name.toLowerCase()}</p>
+                              
+                              <div className="price">
+                                <span className="sale">{Utils.numberToCurrencyBRL(item.sale)}</span>
+                                {
+                                  item.hot &&
+                                  <span className="full">{Utils.numberToCurrencyBRL(item.price)}</span>
+                                }
+                              </div>
+                            </div>
+
+                            <div className="controls">
+                              <div className="wrapper">
+                              <button>-</button>
+                              <span>0</span>
+                              <button>+</button>
+                              </div>
+                            </div>
+
+                          </li>
+                        )
+                      })
+                    }
+                  </ul>
+
                 </div>
-
-                <ul>
-                  {
-                    choice.items.map((item: any, index: number) => {
-                      return (
-                        <li key={index}>
-                          <p><strong>{item.id}</strong> - {item.name} - {item.sale}</p>
-                        </li>
-                      )
-                    })
-                  }
-                </ul>
-
-              </div>
-            )
-          })
-        }
+              )
+            })
+          }
+        </div>
 
       </div>
     </PageDefault>
