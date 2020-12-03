1cc69e304bb97c7521977c744d63119b252b3822
Cesar <cesardmn@gmail.com>
2020-12-03 07:57:43 -0300

    change for only print menu and redirect to main

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,8 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import Routes from './Routes'
-import api from './services/api';
-import LoadingPage from './pages/LoadingPage';
 
 function App() {
-
-  const [isLoading, setIsLoading] = useState(true)
-
-  useEffect(() => {
-    api.get('').then(response => {
-      const fcdb = JSON.stringify(response.data)
-      localStorage.setItem("fcdb", fcdb)
-
-      setIsLoading(false)
-
-    })
-  }, [])
-
-  return isLoading ? <LoadingPage /> : <Routes />
+  return <Routes />
 }
 
 export default App;
--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,11 +1,6 @@
 import React from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
-import ComboPage from './pages/Combo';
-import ComboOptions from './pages/ComboOptions';
-import Home from './pages/Home';
-import ItemPage from './pages/ItemPage';
 import PrintMenu from './pages/PrintMenu';
-
 
 
 function Routes() {
@@ -13,10 +8,10 @@ function Routes() {
     <BrowserRouter >
       <Switch>
         <Route path="/menu" component={PrintMenu} exact />
-        <Route path="/" component={Home} exact />
-        <Route path="/combo" component={ComboPage} exact />
-        <Route path="/combo/:id" component={ComboOptions} exact/>
-        <Route path="/:name" component={ItemPage} exact/>
+        <Route path='/' component={() => {
+          window.location.href = 'https://fogaocaseiro.com.br/';
+          return null;
+        }} />
       </Switch>
     </BrowserRouter>
   )
