994cab018e78c6c57205488e59ec3c2939003134
Cesar <cesardmn@gmail.com>
2021-01-08 06:17:56 -0300

    add redirect

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,19 +1,23 @@
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import Menu from './pages/Menu'
-import Home from './pages/Home'
-
-import CountProvider from './context/Cart'
 
 export default function Routes() {
   return (
     <BrowserRouter>
       <Switch>
-        <CountProvider >
-          <Route path="/" component={Home} exact />
-        </CountProvider>
         <Route path="/menu" component={Menu} />
+
+        <Route path='/' component={() => {
+          window.location.href = '/menu';
+          return null;
+        }} exact />
+
+        <Route path='/admin' component={() => {
+          window.location.href = 'https://backfc.herokuapp.com/admin';
+          return null;
+        }} />
+        
       </Switch>
     </BrowserRouter>
   )
 }
-
